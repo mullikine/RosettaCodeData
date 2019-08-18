2e3c65def21838a8546b4434f3c24e54da41acd3
Shane Mulligan <mullikine@gmail.com>
2019-08-18 13:30:14 +1200

    Sun 18 Aug 2019 13:30:14 NZST

--- Task/Sorting-algorithms-Bubble-sort/C/sorting-algorithms-bubble-sort.c
+++ Task/Sorting-algorithms-Bubble-sort/C/sorting-algorithms-bubble-sort.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
+#include <time.h> // Needed to seed the random number generator
+#include <stdlib.h>
 
-void bubble_sort (int *a, int n) {
-    int i, t, j = n, s = 1;
-    while (s) {
-        s = 0;
-        for (i = 1; i < j; i++) {
-            if (a[i] < a[i - 1]) {
-                t = a[i];
-                a[i] = a[i - 1];
-                a[i - 1] = t;
-                s = 1;
-            }
-        }
-        j--;
-    }
+/* TODO: populate an array with random integers */
+/* int r = rand() */
+
+/* array[element] = rand() % 100 + 1 */
+
+void bubble_sort(int * a, int n) {
+     int i, t, j = n, s = 1;
+     while (s) {
+          s = 0;
+          for (i = 1; i < j; i++) {
+               if (a[i] < a[i - 1]) {
+                    t = a[i];
+                    a[i] = a[i - 1];
+                    a[i - 1] = t;
+                    s = 1;
+               }
+          }
+          j--;
+     }
 }
 
-int main () {
-    int a[] = {4, 65, 2, -31, 0, 99, 2, 83, 782, 1};
-    int n = sizeof a / sizeof a[0];
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
-    bubble_sort(a, n);
-    for (i = 0; i < n; i++)
-        printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
-    return 0;
+int main() {
+     // This will produce the same random numbers during the same second
+     srand(time(NULL));   // Initialization, should only be called once.
+
+     /* int a[] = {4, 65, 2, -31, 0, 99, 2, 83, 782, 1}; */
+     int size = 10;
+     int * elements = malloc(sizeof(int) * size);
+
+     // inizialize
+     for (int i = 0; i < size; ++i)
+          elements[i] = i;
+
+     for (int i = size - 1; i > 0; --i) {
+          // generate random index
+          int w = rand() % i;
+          /* printf("%d",(int)rand()); */
+          // swap items
+          int t = elements[i];
+          elements[i] = elements[w];
+          elements[w] = t;
+     }
+     printf("\n");
+
+     // int size = sizeof elements / sizeof elements[0];
+     int i;
+     for (i = 0; i < size; i++)
+          printf("%d%s", elements[i], i == size - 1 ? "\n" : " ");
+     bubble_sort(elements, size);
+     for (i = 0; i < size; i++)
+          printf("%d%s", elements[i], i == size - 1 ? "\n" : " ");
+     return 0;
 }
