fab41f313eecba83ca534d2d9afb28fbb2c86ed7
Shane Mulligan <mullikine@gmail.com>
2019-08-26 00:10:22 +1200

    26.08.19

--- Task/FizzBuzz/Python/fizzbuzz-1.py
+++ Task/FizzBuzz/Python/fizzbuzz-1.py
@@ -1,9 +1,9 @@
-for i in xrange(1, 101):
+for i in range(1, 101):
     if i % 15 == 0:
-        print "FizzBuzz"
+        print("FizzBuzz")
     elif i % 3 == 0:
-        print "Fizz"
+        print("Fizz")
     elif i % 5 == 0:
-        print "Buzz"
+        print("Buzz")
     else:
-        print i
+        print(i)
--- Task/FizzBuzz/Python/fizzbuzz-6.py
+++ Task/FizzBuzz/Python/fizzbuzz-6.py
@@ -1,13 +1,13 @@
-from itertools import cycle, izip, count, islice
+from itertools import cycle, count, islice
 
 fizzes = cycle([""] * 2 + ["Fizz"])
 buzzes = cycle([""] * 4 + ["Buzz"])
-both = (f + b for f, b in izip(fizzes, buzzes))
+both = (f + b for f, b in zip(fizzes, buzzes))
 
 # if the string is "", yield the number
 # otherwise yield the string
-fizzbuzz = (word or n for word, n in izip(both, count(1)))
+fizzbuzz = (word or n for word, n in zip(both, count(1)))
 
 # print the first 100
 for i in islice(fizzbuzz, 100):
-    print i
+    print(i)
