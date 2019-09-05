a2d326ad2d4bf6e53f603a96c13c2d226ada74cf
Shane Mulligan <mullikine@gmail.com>
2019-09-05 18:49:34 +1200

    05.09.19

--- Task/Regular-expressions/Haskell/regular-expressions-1.hs
+++ Task/Regular-expressions/Haskell/regular-expressions-1.hs
@@ -1,7 +1,11 @@
+-- module MyRe where
+
 import Text.Regex
 
 str = "I am a string"
 
+main :: IO ()
+main = do
 case matchRegex (mkRegex ".*string$") str of
   Just _  -> putStrLn $ "ends with 'string'"
   Nothing -> return ()
